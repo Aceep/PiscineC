dfbe1ed8f0b98b5b010cfc0c6e9c93fbf8517ef6
Aceep <108153485+Aceep@users.noreply.github.com>
2022-07-07 20:40:57 +0200

    long int

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -22,6 +22,12 @@ void	ft_putchar(char c)
 
 void	ft_putnbr(int nb)
 {
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
 	if (nb < 0)
 	{
 		ft_putchar('-');

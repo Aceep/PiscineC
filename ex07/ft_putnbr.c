b5ac5446f19c945427cdd063f67dbbec2b7ae8dd
Aceep <108153485+Aceep@users.noreply.github.com>
2022-07-07 19:00:37 +0200

    ex7

---
+++ ex07/ft_putnbr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alycgaut <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/07 17:46:56 by alycgaut          #+#    #+#             */
+/*   Updated: 2022/07/07 18:44:52 by alycgaut         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putnbr(int nb);
+void	ft_putchar(char c);
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb > 10)
+	{
+		ft_putnbr(nb / 10);
+	}
+	nb = nb % 10 + 48;
+	ft_putchar(nb);
+}

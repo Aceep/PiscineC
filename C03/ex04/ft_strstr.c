e94f432276fe089c598323ff8d28549d048ac258
Aceep <108153485+Aceep@users.noreply.github.com>
2022-07-13 23:27:20 +0200

    Update ft_strstr.c

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -6,33 +6,19 @@ char	*ft_strstr(char *str, char *to_find)
 {
 	int	i;
 	int	j;
-	int	c;
-	int	n;
 
 	j = 0;
 	i = 0;
+	if  (*to_find == '\0')
+		retrun (str);
 	while (str[i])
 	{
-		if (str[i] == to_find[j])
-		{
-			while  (str[i + j] == to_find[j])
-			{
-				j ++;
-			}
-		}
+		while  (str[i + j] == to_find[j] && str[i + j] != '\0')
+			j ++;
 		if (to_find[j] == '\0' )
-		{
 			return (&str[i]);
-		}
 		j = 0;
 		i ++;
 	}
-}
-
-int	main()
-{
-	char *str = "Il etait une fois, une famille qui vivait dans les bois";
-	char *to_find = "tait";
-	
-	printf("%s", ft_strstr(str, to_find));
+	return (0);
 }

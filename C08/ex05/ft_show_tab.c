cba5869b4c8fa5423ae183dd22e5070b33c0a775
Alycia GAUTIER <alycgaut@e3r11p19.clusters.42paris.fr>
2022-07-21 19:12:36 +0200

    fin

--- C08/ex05/ft_show_tab.c
+++ C08/ex05/ft_show_tab.c
@@ -1,33 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alycgaut <alycgaut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/21 17:30:47 by alycgaut          #+#    #+#             */
-/*   Updated: 2022/07/21 19:12:05 by alycgaut         ###   ########.fr       */
+/*   Created: 2022/07/21 17:30:18 by alycgaut          #+#    #+#             */
+/*   Updated: 2022/07/21 18:29:45 by alycgaut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-
-int 	strlen(char *av)
-{
-	int 	i;
-}
-
-struct s_stock_str *ft_strs_to_tab(int ac, char **av)
-{	
-	int 	i;
-
-	i = 1;
-	t_stock_str tab;
-
-	tab.size = ft_strlen(av[i]);
-	tab.str = av[i];
-	tab.copy = ft_strcpy(av[i]);
-
-	return ();
-}
-
 

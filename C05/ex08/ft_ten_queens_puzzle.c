16d1c31a0dc2a7f5b8b40ca01400b11d7dd0197c
Aceep <alycia.gautier@laposte.net>
2022-07-21 08:05:17 +0200

    quueens

--- C05/ex08/ft_ten_queens_puzzle.c
+++ C05/ex08/ft_ten_queens_puzzle.c
@@ -10,14 +10,80 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdio.h>
 
-int	ft_ten_queens_puzzle(void)
+void	ft_putchar(int tab)
 {
-	char	tab[10];
+	char	c;
 
+	c = tab + '0';
+	write(1, &c, 1);
 }
 
+void	ft_display_soluce(int *tab, int column, int *nb_soluce)
+{
+	column = 0;
+	while (column < 10)
+	{
+		ft_putchar(tab[column]);
+		column = column + 1;
+	}
+	write(1, "\n", 1);
+	*nb_soluce = *nb_soluce + 1;
+}
+
+int		ft_position_is_valid(int *tab, int column, int queen_case)
+{
+	int		i;
+
+	i = 0;
+	while (i < column)
+	{
+		if (tab[i] == queen_case )
+				return (0);
+		i ++;
+	}
+	i = 0;
+	while (i < column)
+	{
+
+		if (tab[i] - i == queen_case - column || tab[i] + i == queen_case + column)
+				return (0);
+		i ++;
+	}
+	return (1);
+}
+
+void	ft_find_soluce(int *tab, int column, int *nb_soluce)
+{
+	if (column == 10)
+	{
+			ft_display_soluce(tab, column, &(*nb_soluce));
+			return ;
+	}
+	tab[column] = 0;
+	while (tab[column] < 10)
+	{
+		if (ft_position_is_valid(tab, column, tab[column]))
+				ft_find_soluce(tab, column + 1, &(*nb_soluce));
+		tab[column]++;
+	}
+}
+
+int		ft_ten_queens_puzzle(void)
+{
+	int		tab[10];
+	int		nb_soluce;
+
+	nb_soluce = 0;
+	ft_find_soluce(tab, 0, &nb_soluce);
+	return (nb_soluce);
+}
 int	main(void)
 {
-	ft_ten_queens_puzzle();
+	int 	max;
+
+	max = ft_ten_queens_puzzle();
+	printf("%d", max);
 }

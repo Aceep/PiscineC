c5de84c31152bd8b0615d80892f07a0a29f544e7
Alycia GAUTIER <alycgaut@e3r10p1.clusters.42paris.fr>
2022-07-22 13:17:57 +0200

    finC08

--- C08/ex04/ft_strs_to_tab.c
+++ C08/ex04/ft_strs_to_tab.c
@@ -6,28 +6,57 @@
 /*   By: alycgaut <alycgaut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 17:30:47 by alycgaut          #+#    #+#             */
-/*   Updated: 2022/07/21 19:12:05 by alycgaut         ###   ########.fr       */
+/*   Updated: 2022/07/22 12:54:50 by alycgaut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include "ft_stock_str.h"
 
-int 	strlen(char *av)
+int	ft_strlen(char *av)
 {
-	int 	i;
+	int	i;
+
+	i = 0;
+	while (av[i])
+		i ++;
+	return (i);
 }
 
-struct s_stock_str *ft_strs_to_tab(int ac, char **av)
-{	
-	int 	i;
+char	*ft_strdup(char *av, int size)
+{
+	int		i;
+	char	*dest;
 
-	i = 1;
-	t_stock_str tab;
-
-	tab.size = ft_strlen(av[i]);
-	tab.str = av[i];
-	tab.copy = ft_strcpy(av[i]);
-
-	return ();
+	i = 0;
+	dest = malloc(sizeof(char) * size + 1);
+	if (!dest)
+		return (NULL);
+	while (av[i])
+	{
+			dest[i] = av[i];
+			i ++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 
+struct	s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	int			i;
+	t_stock_str	*tab;
 
+	i = 0;
+	tab = malloc(sizeof(t_stock_str) * (ac + 1));
+	if (!tab)
+		return (NULL);
+	while (i < ac)
+	{
+		tab[i].size = ft_strlen(av[i]);
+		tab[i].str = av[i];
+		tab[i].copy = ft_strdup(av[i], ft_strlen(av[i]));
+		i ++;
+	}
+	tab[i].str = 0;
+	return (tab);
+}
--- C08/ex05/ft_show_tab.c
+++ C08/ex05/ft_show_tab.c
@@ -6,7 +6,45 @@
 /*   By: alycgaut <alycgaut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 17:30:18 by alycgaut          #+#    #+#             */
-/*   Updated: 2022/07/21 18:29:45 by alycgaut         ###   ########.fr       */
+/*   Updated: 2022/07/22 13:15:57 by alycgaut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_stock_str.h"
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i ++;
+	}
+	write(1, "\n", 1);
+}
+
+void	ft_putchar(int size)
+{
+	char	c;
+
+	c = size + '0';
+	write(1, &c, 1);
+	write(1, "\n", 1);
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int	i;
+
+	i = 0;
+	while (par[i].str != 0)
+	{
+		ft_putstr(par[i].str);
+		ft_putchar(par[i].size);
+		ft_putstr(par[i].copy);
+		i ++;
+	}
+}
